e170c66f59ff9691f7038633ed81af6c9409c8fe
Mitchell Zinck <mitchellfzinck@gmail.com>
2024-09-04 16:45:30 -0300

    Initial Commit

---
+++ FriendSocial-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendSocial-IOS
+//
+//  Created by Mitchell Zinck on 2024-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendSocial-IOS/FriendSocial_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendSocial_IOSApp.swift
+//  FriendSocial-IOS
+//
+//  Created by Mitchell Zinck on 2024-09-04.
+//
+
+import SwiftUI
+
+@main
+struct FriendSocial_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
